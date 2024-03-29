6887a264e4b8c474d3a46fbcc2db506a37f3bfc6
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-03-29 03:33:35 +0100

    Implement self-joins in Employees and Managers assignment

---
+++ EmployeesAndManagers/src/test/java/com/employees/managers/java/EmployeesAndManagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employees.managers.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesAndManagersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
